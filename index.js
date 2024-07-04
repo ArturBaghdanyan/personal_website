d451b206d02c27c55885b1b2cfcf6838314d0b2b
ArturBaghdanyan <arturbaghdanyan20@gmail.com>
2024-07-04 13:57:47 +0400

    added animations

--- index.js
+++ index.js
@@ -128,8 +128,41 @@ const data = {
     ]
 };
 
+const sections = document.querySelectorAll('.columns');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+
+    if(top >= offset && top <= offset + height) {
+      sec.classList.add('show-animate')
+    }
+    else {
+      sec.classList.remove('show-animate')
+    }
+  })
+}
+
+const navLinks = document.querySelectorAll('.nav > a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', function() {
+    navLinks.forEach(link => {
+      link.classList.remove('active');
+    });
+    
+    this.classList.add('active');
+  });
+});
+
+
+
+const header = document.getElementById('header');
+
 window.addEventListener('scroll', () => {
-  const header = document.getElementById('header');
+
   if (window.scrollY > 0) {
     header.classList.add('sticky');
   } else {
@@ -140,12 +173,16 @@ window.addEventListener('scroll', () => {
 function showSideBar() {
     const nav = document.getElementById("header-nav");
     nav.style.display = 'flex';
+    nav.style.transition = 'right 2s';
+    nav.classList.add('open');
 }
   
 function hideSidebar() {
   const nav = document.getElementById("header-nav");
-  nav.style.display = 'none';
+  nav.style.transition = 'right 2s'; 
+  nav.classList.remove('open');
 }
+
 
 let toggleIcons  = document.querySelectorAll("#icon");
 const body = document.querySelector('body');
@@ -248,9 +285,9 @@ data.work.forEach((project, index) => {
 			<h2>${project.title}</h2>
 			<p>${project.description}</p>
 			<div class="project_description">${itemsHTML}</div>
-			<div class="project_skills-icon">
+			<button class="project_skills-icon">
 				<img src="${project.skillImgSrc}" alt="skill">
-			</div>
+			</button>
 		</div>
 	</div>
 	`
